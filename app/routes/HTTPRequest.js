ec2fb5d258053e5ff10ac7ae08c6209f7de3fcd1
david855033 <david855033@gmail.com>
2017-07-08 22:16:21 +0800

    setting route

--- app/routes/HTTPRequest.js
+++ app/routes/HTTPRequest.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var getJSON = require('./getJSON');
 var http = require("http");
 
 /* get HTML */
 router.post('/', function(req, res, next) {
     var options=req.body;
-    console.log("http request! option sended:\n"+ JSON.stringify(options));
+    console.log("http request! option sended: "+ JSON.stringify(options));
 
       var httpConnection = http.request(options,
           function(httpResponse){
               var output="";
-              console.log('httpReq');
               
               httpResponse.on('data',function(chunk){
                 output+=chunk;
@@ -23,7 +21,7 @@ router.post('/', function(req, res, next) {
           });
 
       httpConnection.on('error',function(err){
-        console.log('ERR');
+        console.log('ERR '+err.stack);
           res.send("ERR");
       });
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var http = require("http");
+var https = require("https");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/*', function(req, res, next) { 
+  if(req.baseUrl&&(req.baseUrl.indexOf("remote")>=0||req.baseUrl.indexOf("favicon")>=0))
+  {
+    next();
+  }
+
+  console.log(req.baseUrl);
+  var options=
+  {
+      hostname:"localhost",
+      path: req.baseUrl,
+      port:3010
+  };
+
+  var contentType="text/html";
+  if(req.baseUrl.indexOf(".js")>=0)
+  {
+    contentType='application/x-javascript';
+  }else if(req.baseUrl.indexOf(".css")>=0)
+  {
+    contentType='text/css';
+  }
+  res.setHeader('content-type', contentType);
+
+  var httpConnection = http.request(options,
+          function(httpResponse){
+              var output="";
+              httpResponse.on('data',function(chunk){
+                output+=chunk;
+              });
+              httpResponse.on('end',function(){
+                res.end(output);
+              });
+          });
+
+      httpConnection.on('error',function(err){
+        res.setHeader('content-type', 'application/x-javascript');
+        res.end("console.log('ERR "+req.baseUrl+"');");
+      });
+
+      httpConnection.end();
 });
 
 module.exports = router;
