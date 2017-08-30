75e99b4b5fb633f589add6da37c3189eee595f89
Lo Yu-Cheng <david855033@gmail.com>
2017-08-30 23:25:16 +0800

    .

--- app/public/remote/layout.js
+++ app/public/remote/layout.js
@@ -2,21 +2,36 @@
 var $flowsheetContent=$("#flow-sheet .content");
 var $flowsheetHeader=$("#flow-sheet .header");
 var $flowsheetFootBar=$("#flow-sheet .footBar");
+var $flowsheetFootBarCard=$("#flow-sheet .footBarCard");
 var $window=$(window);
 
-$(function(){
-    setTPRHeight();
-    setFootbarHeight(300);
-});
-$(window).resize(function(){
-    setTPRHeight();
-});
-
-var setTPRHeight=function(){
-    $flowsheetContent.height($window.height()-$flowsheetHeader.height()-20-$flowsheetFootBar.height());
+var Layout={};
+Layout.footBar={};
+Layout.footBar.FOOT_BAR_MAX_HEIGHT=240;
+Layout.footBar.FOOT_BAR_MIN_HEIGHT=40;
+Layout.footBar.currentHeight = 240;
+Layout.footBar.open=function(){
+    Layout.footBar.setHeight(Layout.footBar.FOOT_BAR_MAX_HEIGHT);
+};
+Layout.footBar.close=function(){
+    Layout.footBar.setHeight(Layout.footBar.FOOT_BAR_MIN_HEIGHT);
+};
+Layout.footBar.setHeight=function(height){
+    Layout.footBar.currentHeight=height;
+    $flowsheetFootBar.height(Layout.footBar.currentHeight);
+    $flowsheetContent.height($window.height()-$flowsheetHeader.height()-20-Layout.footBar.currentHeight);
+};
+Layout.footBar.calculateCardWidth=function(){
+    $flowsheetFootBarCard.width($flowsheetFootBar.width()/2-2);
 };
 
-var setFootbarHeight=function(height){
-     $flowsheetFootBar.height(height);
-     setTPRHeight();
-}
+
+$(function(){
+    Layout.footBar.open();
+    Layout.footBar.calculateCardWidth();
+});
+
+$(window).resize(function(){
+    Layout.footBar.setHeight(Layout.footBar.currentHeight);
+    Layout.footBar.calculateCardWidth();
+});
--- app/public/remote/view.js
+++ app/public/remote/view.js
@@ -5,81 +5,96 @@ var view= new Vue({
         account:"DOC3924B",
         password:"888888",
         cookie:"",
+        viewList:['dev','flow-sheet'],
         selectedView:"flow-sheet",
         wardList:["NICU","PICU"],
         patientList:{content:[], timeStamp:""},
         selectedPatientID:"",
         selectedCaseNo:"",
         admissionList:{content:[], timeStamp:""},
-        patientData:{
-            content:{
-                currentBed:"",
-                patientName:"",
-                birthDate:"",
-                gender:"",
-                bloodType:"",
-                currentSection:"",
-                visitingStaff:{name:"",code:""},
-                resident:{name:"",code:""}
+        dev:{
+            patientData:{
+                content:{
+                    currentBed:"",
+                    patientName:"",
+                    birthDate:"",
+                    gender:"",
+                    bloodType:"",
+                    currentSection:"",
+                    visitingStaff:{name:"",code:""},
+                    resident:{name:"",code:""}
+                },
+                timeStamp:""
             },
-            timeStamp:""
+            changeBedSection:{
+                content:{changeBed:[],changedSection:[]},
+                timeStamp:""
+            },
+            consultation:{
+                content:[],
+                timeStamp:""
+            },
+            consultationReply:{
+                content:"",
+                timeStamp:""
+            },
+            consultationPending:{
+                content:[],
+                timeStamp:""
+            },
+            surgery:{
+                content:[],
+                timeStamp:""
+            },
+            order:{
+                content:[],
+                timeStamp:""
+            },
+            report:{
+                content:[],
+                timeStamp:""
+            },
+            reportContent:{
+                content:{htmlText:"",parsed:[]},
+                timeStamp:""
+            },
+            cummulative:{
+                content:{colNames:[], data:[]},
+                timeStamp:""
+            },
+            availableCummulativeList:[
+                {queryString:"DCHEM",item:"SMAC"},
+                {queryString:"DCBC",item:"CBC"},
+                {queryString:"DGLU1",item:"床邊血糖"},
+                {queryString:"DBGAS",item:"BloodGas"}
+            ],
+            selectedCummulativeList:"DCHEM",
+            vitalSign:{
+                content:{colNames:[], data:[]},
+                timeStamp:""
+            },
+            availableVitalSignList:[
+                {queryString:"HWS",item:"身高體重"},
+                {queryString:"BPP",item:"血壓脈搏"},
+                {queryString:"IO",item:"輸入輸出"},
+                {queryString:"TMP",item:"體溫"},
+                {queryString:"RSP",item:"呼吸"},
+                {queryString:"OXY",item:"血氧濃度"}
+            ],
+            selectedVitalSignList:"HWS"
         },
-        changeBedSection:{
-            content:{changeBed:[],changedSection:[]},
-            timeStamp:""
-        },
-        consultation:{
-            content:[],
-            timeStamp:""
-        },
-        consultationReply:{
-            content:"",
-            timeStamp:""
-        },
-        consultationPending:{
-            content:[],
-            timeStamp:""
-        },
-        surgery:{
-            content:[],
-            timeStamp:""
-        },
-        order:{
-            content:[],
-            timeStamp:""
-        },
-        report:{
-            content:[],
-            timeStamp:""
-        },
-        reportContent:{
-            content:{htmlText:"",parsed:[]},
-            timeStamp:""
-        },
-        cummulative:{
-            content:{colNames:[], data:[]},
-            timeStamp:""
-        },
-        availableCummulativeList:[
-            {queryString:"DCHEM",item:"SMAC"},
-            {queryString:"DCBC",item:"CBC"},
-            {queryString:"DGLU1",item:"床邊血糖"},
-            {queryString:"DBGAS",item:"BloodGas"}
-        ],
-        selectedCummulativeList:"DCHEM",
-        vitalSign:{
-            content:{colNames:[], data:[]},
-            timeStamp:""
-        },
-        availableVitalSignList:[
-            {queryString:"HWS",item:"身高體重"},
-            {queryString:"BPP",item:"血壓脈搏"},
-            {queryString:"IO",item:"輸入輸出"},
-            {queryString:"TMP",item:"體溫"},
-            {queryString:"RSP",item:"呼吸"},
-            {queryString:"OXY",item:"血氧濃度"}
-        ],
-        selectedVitalSignList:"HWS"
+        flowSheet:{
+            showFootBar:true
+        }
+    },
+    watch:{
+        'flowSheet.showFootBar':function(){
+            if(view.flowSheet.showFootBar){
+                Layout.footBar.open();
+            }else{
+                Layout.footBar.close();
+            }
+        }
     },
     methods:{
         signIn:function(callback){
@@ -91,96 +106,99 @@ var view= new Vue({
                 view.patientList.timeStamp = timeStamp;
             });
         },
-        updatePatient:function(patientID){
-            view.updateAdmissionList(patientID);
-            view.selectedPatientID=patientID;
-            view.updatePatientData(patientID)
-            view.updateChangeBedSection(patientID);
-            view.updateConsultation(patientID);
-            view.updateConsultationPending(patientID);
-            view.updateSurgery(patientID);
-            view.updateOrder(patientID,7);
-            view.updateReport(patientID, 1);
-            view.updateCummulative(patientID,3,view.selectedCummulativeList);
-        },
-        updateCase:function(patientID, caseNo){
-            patientID=patientID||view.selectedPatientID;
-            view.selectedCaseNo=caseNo;
-            view.updateVitalSign(patientID, caseNo, view.selectedVitalSignList)
-        },
         updateAdmissionList:function(patientID){
             requestAdmissionList(patientID,function(data,timeStamp){
                 view.admissionList.content = data;
                 view.admissionList.timeStamp = timeStamp;
                 view.selectedCaseNo=view.admissionList.content&&view.admissionList.content[0].caseNo;
-                view.updateCase(patientID, view.selectedCaseNo);
+                view.devMethod.updateCase(patientID, view.selectedCaseNo);    //for dev show
             });
         },
-        updatePatientData:function(patientID){
-            requestPatientData(patientID,function(data,timeStamp){
-                view.patientData.content = data;
-                view.patientData.timeStamp = timeStamp;
-            });
-        },
-        updateChangeBedSection:function(patientID){
-            requestChangeBedSection(patientID,function(data,timeStamp){
-                view.changeBedSection.content=data;
-                view.changeBedSection.timeStamp=timeStamp;
-            });
-        },
-        updateConsultation:function(patientID){
-            requestConsultation(patientID,function(data,timeStamp){
-                view.consultation.content=data;
-                view.consultation.timeStamp=timeStamp;
-            });
-        },
-        updateConsultationReply:function(patientID,caseNo, oseq){
-            requestConsultationReply(patientID,caseNo, oseq, function(data,timeStamp){
-                view.consultationReply.content=data;
-                view.consultationReply.timeStamp=timeStamp;
-            });
-        },
-        updateConsultationPending:function(patientID){
-            requestConsultationPending(patientID,function(data,timeStamp){
-                view.consultationPending.content=data;
-                view.consultationPending.timeStamp=timeStamp;
-            });
-        },
-        updateSurgery:function(patientID){
-            requestSurgery(patientID,function(data,timeStamp){
-                view.surgery.content=data;
-                view.surgery.timeStamp=timeStamp;
-            });
-        },
-        updateOrder:function(patientID, days){
-            requestOrder(patientID,days,function(data,timeStamp){
-                view.order.content=data;
-                view.order.timeStamp=timeStamp;
-            });
-        },
-        updateReport:function(patientID, monthsOrYear){
-            requestReport(patientID,monthsOrYear,function(data,timeStamp){
-                view.report.content=data;
-                view.report.timeStamp=timeStamp;
-            });
-        },
-        updateReportContent:function(patientID,partNo,caseNo, orderSeq){
-            requestReportContent(patientID,partNo,caseNo, orderSeq,function(data,timeStamp){
-                view.reportContent.content=data;
-                view.reportContent.timeStamp=timeStamp;
-            });
-        },
-        updateCummulative:function(patientID,monthsOrYear, field){
-            requestCummulative(patientID,monthsOrYear,field,function(data,timeStamp){
-                view.cummulative.content=data;
-                view.cummulative.timeStamp=timeStamp;
-            });
-        },
-        updateVitalSign:function(patientID,caseNo, field){
-            requestVitalSign(patientID,caseNo, field,function(data,timeStamp){
-                view.vitalSign.content=data;
-                view.vitalSign.timeStamp=timeStamp;
-            });
+        devMethod:{
+            updatePatient:function(patientID){
+                view.updateAdmissionList(patientID);
+                view.selectedPatientID=patientID;
+                view.devMethod.updatePatientData(patientID)
+                view.devMethod.updateChangeBedSection(patientID);
+                view.devMethod.updateConsultation(patientID);
+                view.devMethod.updateConsultationPending(patientID);
+                view.devMethod.updateSurgery(patientID);
+                view.devMethod.updateOrder(patientID,7);
+                view.devMethod.updateReport(patientID, 1);
+                view.devMethod.updateCummulative(patientID,3,view.dev.selectedCummulativeList);
+            },
+            updateCase:function(patientID, caseNo){
+                patientID=patientID||view.selectedPatientID;
+                view.selectedCaseNo=caseNo;
+                view.devMethod.updateVitalSign(patientID, caseNo, view.dev.selectedVitalSignList)
+            },
+        
+            updatePatientData:function(patientID){
+                requestPatientData(patientID,function(data,timeStamp){
+                    view.dev.patientData.content = data;
+                    view.dev.patientData.timeStamp = timeStamp;
+                });
+            },
+            updateChangeBedSection:function(patientID){
+                requestChangeBedSection(patientID,function(data,timeStamp){
+                    view.dev.changeBedSection.content=data;
+                    view.dev.changeBedSection.timeStamp=timeStamp;
+                });
+            },
+            updateConsultation:function(patientID){
+                requestConsultation(patientID,function(data,timeStamp){
+                    view.dev.consultation.content=data;
+                    view.dev.consultation.timeStamp=timeStamp;
+                });
+            },
+            updateConsultationReply:function(patientID,caseNo, oseq){
+                requestConsultationReply(patientID,caseNo, oseq, function(data,timeStamp){
+                    view.dev.consultationReply.content=data;
+                    view.dev.consultationReply.timeStamp=timeStamp;
+                });
+            },
+            updateConsultationPending:function(patientID){
+                requestConsultationPending(patientID,function(data,timeStamp){
+                    view.dev.consultationPending.content=data;
+                    view.dev.consultationPending.timeStamp=timeStamp;
+                });
+            },
+            updateSurgery:function(patientID){
+                requestSurgery(patientID,function(data,timeStamp){
+                    view.dev.surgery.content=data;
+                    view.dev.surgery.timeStamp=timeStamp;
+                });
+            },
+            updateOrder:function(patientID, days){
+                requestOrder(patientID,days,function(data,timeStamp){
+                    view.dev.order.content=data;
+                    view.dev.order.timeStamp=timeStamp;
+                });
+            },
+            updateReport:function(patientID, monthsOrYear){
+                requestReport(patientID,monthsOrYear,function(data,timeStamp){
+                    view.dev.report.content=data;
+                    view.dev.report.timeStamp=timeStamp;
+                });
+            },
+            updateReportContent:function(patientID,partNo,caseNo, orderSeq){
+                requestReportContent(patientID,partNo,caseNo, orderSeq,function(data,timeStamp){
+                    view.dev.reportContent.content=data;
+                    view.dev.reportContent.timeStamp=timeStamp;
+                });
+            },
+            updateCummulative:function(patientID,monthsOrYear, field){
+                requestCummulative(patientID,monthsOrYear,field,function(data,timeStamp){
+                    view.dev.cummulative.content=data;
+                    view.dev.cummulative.timeStamp=timeStamp;
+                });
+            },
+            updateVitalSign:function(patientID,caseNo, field){
+                requestVitalSign(patientID,caseNo, field,function(data,timeStamp){
+                    view.dev.vitalSign.content=data;
+                    view.dev.vitalSign.timeStamp=timeStamp;
+                });
+            }            
         }
     }
 })
